d38436b66c860db1fc75486464b0befbc678b639
9swampy <github@prims.com>
2014-08-28 00:26:10 +0100

    Package for NuGet publish.

--- InpcTracer.Nuget.Tests/Properties/AssemblyInfo.cs
+++ InpcTracer.Nuget.Tests/Properties/AssemblyInfo.cs
@@ -5,12 +5,12 @@ using System.Runtime.InteropServices;
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
 // associated with an assembly.
-[assembly: AssemblyTitle("InpcTracer")]
-[assembly: AssemblyDescription("A fluent interface for unit testing INotifyPropertyChanged events")]
+[assembly: AssemblyTitle("InpcTracer.Nuget.Tests")]
+[assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
-[assembly: AssemblyCompany("InpcTracer contributors")]
-[assembly: AssemblyProduct("InpcTracer")]
-[assembly: AssemblyCopyright("MIT")]
+[assembly: AssemblyCompany("")]
+[assembly: AssemblyProduct("InpcTracer.Nuget.Tests")]
+[assembly: AssemblyCopyright("Copyright ©  2014")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
 
@@ -20,7 +20,7 @@ using System.Runtime.InteropServices;
 [assembly: ComVisible(false)]
 
 // The following GUID is for the ID of the typelib if this project is exposed to COM
-[assembly: Guid("908fa673-57b4-4eca-9bf6-eb5b1e40abeb")]
+[assembly: Guid("8a496939-48b3-42b5-9f59-6b3901e0e911")]
 
 // Version information for an assembly consists of the following four values:
 //
@@ -32,5 +32,5 @@ using System.Runtime.InteropServices;
 // You can specify all the values or you can default the Build and Revision Numbers 
 // by using the '*' as shown below:
 // [assembly: AssemblyVersion("1.0.*")]
-[assembly: AssemblyVersion("0.0.1.*")]
-[assembly: AssemblyFileVersion("0.0.0.1")]
+[assembly: AssemblyVersion("1.0.0.0")]
+[assembly: AssemblyFileVersion("1.0.0.0")]
---
+++ InpcTracer.Nuget.Tests/Sample.cs
@@ -0,0 +1,39 @@
+﻿namespace InpcTracer.Nuget.Tests
+{
+  using System;
+  using Microsoft.VisualStudio.TestTools.UnitTesting;
+  using FakeItEasy;
+  using System.ComponentModel;
+
+  [TestClass]
+  public class Sample
+  {
+    public interface ITraceable : INotifyPropertyChanged
+    {
+      bool Active { get; set; }
+    }
+
+    [TestMethod]
+    public void Test_Notify_Property_Changed_Fired()
+    {
+      ITraceable target = A.Fake<ITraceable>();
+      A.CallTo(target).Where(x => x.Method.Name == "set_Active")
+                .Invokes(() =>
+                {
+                  target.PropertyChanged += Raise.With(new PropertyChangedEventArgs("Active")).Now;
+                });
+
+      var tracer = new InpcTracer.InpcTracer<ITraceable>(target);
+
+      // Check for one event
+      Assert.IsTrue(tracer.WhileProcessing(() => target.Active = true).HasRecordedEvent(() => target.Active).ExactlyOnce());
+
+      // Check for exact order of two events
+      tracer.WhileProcessing(() =>
+      {
+        target.Active = false;
+        target.Active = true;
+      }).FirstRecordedEvent(() => target.Active).ThenRecordedEvent(() => target.Active);
+    }
+  }
+}
